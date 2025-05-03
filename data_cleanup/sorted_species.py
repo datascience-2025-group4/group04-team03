7a509c282141204ab67f98f538336a701be4ad27
sandrabarth4 <sandra.barth@stud.uni-heidelberg.de>
2025-05-03 13:05:24 +0200

    update_sorted_species

--- data_cleanup/sorted_species.py
+++ data_cleanup/sorted_species.py
@@ -1,10 +1,11 @@
 import pandas as pd
 table_antigen_species = pd.read_csv("data/ab_ag.tsv", sep = "\t")
-table_sorted = table_antigen_species["antigen_species"].unique()
-print(table_sorted)
 
 name_counts = table_antigen_species["antigen_species"].value_counts().sort_index()
-print(name_counts)
 name_counts_df = name_counts.reset_index()
 name_counts_df.columns = ['antigen_species', 'count']
+pd.set_option('display.max_rows', None) 
+pd.set_option('display.max_columns', None)  
+pd.set_option('display.width', None)  
+pd.set_option('display.max_colwidth', None)
 print(name_counts_df)
